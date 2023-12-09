f8b339047f3501673fe320bac8c26b7c8ad9af2e
Kot <kot@yukata.dev>
2023-12-09 00:09:48 -0600

    Day 9 - OASIS!!

---
+++ 2023/9-oasis/1-history.py
@@ -0,0 +1,27 @@
+import itertools
+
+with open("./input.txt", "r") as file:
+    s = 0
+    for line in file:
+        values = list(map(int, line.split())) 
+        diffs = [values]
+        while not all(v == 0 for v in diffs[-1]):
+            diff = []
+            for a, b in itertools.pairwise(diffs[-1]):
+                diff.append(b - a)
+            diffs.append(diff)
+        del diff
+
+        diffs[-1].append(0)
+        # print(diffs)
+
+        for idx in range(len(diffs)-2, -1, -1):
+            print("i", idx)
+            x = diffs[idx][-1] + diffs[idx+1][-1]
+            print("x", x)
+            diffs[idx].append(x)
+
+        # print(diffs)
+        s += x
+
+    print(s)
